755c17993894829fae1bc891d7fa3d93726f8c3e
UK-RANJITH <116745760+UK-RANJITH@users.noreply.github.com>
2022-10-26 21:29:34 +0530

    Add files via upload

---
+++ day1/SecondCode.java
@@ -0,0 +1,33 @@
+package week1.day1;
+
+public class SecondCode {
+
+	
+public void phoneNum()
+{
+	}
+
+
+	public int getPhoneNum()
+	{
+		return 123456789;
+	}
+
+	public char getPhoneCol()
+	{
+		return 'r';
+	}
+	
+    public String phonenum()
+    {
+    	return "fdfd";
+	
+    }
+    public static void main(String[] args) {
+		SecondCode obj=new SecondCode();
+		int phoneNum = obj.getPhoneNum();
+		System.out.println(phoneNum);
+		char phoneCol = obj.getPhoneCol();
+		System.out.println(phoneCol);
+	}
+    }
